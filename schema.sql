1593b12150ccba283cde984445cec31f962db8bf
unknown <godly.mg2@gmail.com>
2019-03-13 22:22:17 -0400

    schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
+DROP DATABASE IF EXISTS expense_db;
 -- Creates the "todolist" database --
-CREATE DATABASE todolist;
+CREATE DATABASE expense_db;
